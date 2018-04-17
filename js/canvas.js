771f89b25cda61c9847869e9a77fe5e1bed6b4ea
lo1cgsan <lo1cgsan@gmail.com>
2018-04-17 20:44:43 +0200

    wstawianie i właściwości tekstu

--- js/canvas.js
+++ js/canvas.js
@@ -12,3 +12,22 @@ ctx.clearRect(45,45,60,60);
 //rysujemy obramowanie drugiego kwadratu
 //x, y, width, height
 ctx.strokeRect(50,50,50,50);
+
+//określenie czcionki
+ctx.font = "italic bold 30px Arial";
+// wyrównanie tekstu w pionie: top, hanging, middle, alphabetic, ideographic, bottom
+ctx.textBaseline = "middle";
+// wypełniony tekst w x, y
+ctx.fillText('Ala ma kota 1', 0, 145);
+
+ctx.font = "italic bold 20px Arial";
+ctx.textBaseline = "bottom";
+// wyrównannie tekstu w poziomie: start, end, left, right, center
+ctx.textAlign = "center";
+// obrsowany tekst w x, y
+ctx.strokeText('Ala ma kota 2', 70, 180);
+
+ctx.font = "normal 10px Arial";
+ctx.textBaseline = "top";
+ctx.textAlign = "right";
+ctx.strokeText('Ala ma kota 3', 80, 190);
