15e698a2af47eed04ec8a4c2760d9b22a27a030f
lo1cgsan <lo1cgsan@gmail.com>
2018-04-17 20:32:21 +0200

    canvas fillRect, clearRect

---
+++ js/canvas.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+const canvasElem = document.getElementById('canvas');
+const ctx = canvasElem.getContext('2d');
+
+//rysujemy niebieski kwadrat
+//x, y, width, height
+ctx.fillRect(25,25,100,100);
+
+//wycinamy jego srodek
+ctx.clearRect(45,45,60,60);
+
+//rysujemy obramowanie drugiego kwadratu
+//x, y, width, height
+//ctx.strokeRect(50,50,50,50);
